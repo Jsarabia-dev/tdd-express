dc868c3624bd4d41289db9053b03d7649541d876
JSarabia-dev <jsarabia.dev@gmail.com>
2021-11-18 11:51:33 -0300

    add userMiddleware and UserRegister

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -7,7 +7,7 @@ const { sequelize } = require('../src/config/database');
 beforeAll( () => sequelize.sync());
 beforeEach(() => User.destroy({ truncate: true }));
 
-const postValidUser = (username = 'user_default', email = 'user_default@gmail.com', password = 'password_default') => request(app)
+const postUser = (username = 'user_default', email = 'user_default@gmail.com', password = 'password_default') => request(app)
   .post('/api/1.0/users')
   .send({
     username,
@@ -17,23 +17,23 @@ const postValidUser = (username = 'user_default', email = 'user_default@gmail.co
 
 describe('User registration', () => {
   it('Returns 200 OK when sigun request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.status).toBe(200);
   });
 
   it('Returns sucess message when sigup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.body.message).toBe('User created');
   });
 
   it('Save user to DB', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     expect(userList.length).toBe(1);
   });
 
   it('Save username an email to DB aaa', async () => {
-    await postValidUser('user2', 'user2@gmail.com');
+    await postUser('user2', 'user2@gmail.com');
     const userList = await User.findAll();
     const userSave = userList[0];
     expect(userSave.username).toBe('user2');
@@ -41,9 +41,34 @@ describe('User registration', () => {
   });
 
   it('Hashes te password in DB', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     const userSave = userList[0];
     expect(userSave.password).not.toBe('password_default');
+  });
+
+  it('Return 400 when usernames is null', async () => {
+    const response = await postUser(null);
+    expect(response.status).toBe(400);
+  });
+
+  it('Return validationsErrors field in response body when validation error occurrs', async () => {
+    const response = await postUser(null);
+    expect(response.body.validationsErrors).not.toBeUndefined();
+  });
+
+  it('Return validationsErrors.username field in response body when username is null', async () => {
+    const response = await postUser(null);
+    expect(response.body.validationsErrors.username).toBe('Username cannot be null');
+  });
+
+  it('Return validationsErrors.email field in response body when email is null', async () => {
+    const response = await postUser('aaa', null);
+    expect(response.body.validationsErrors.email).toBe('Email cannot be null');
+  });
+
+  it('Return erros for both when username and email is null', async () => {
+    const { body } = await postUser(null, null);
+    expect(Object.keys(body.validationsErrors)).toEqual( ['username', 'email']);
   });
 });
--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -4,11 +4,24 @@ const UserService = require('./UserService');
 
 const router = express.Router();
 
-router.post('/api/1.0/users', async (req, res) => {
-  console.log('[+] BODY -> ', req.body);
-  const userSaved = await UserService.save(req.body);
+const userValidatorMiddleware = (req, res, next) => {
+  let validationsErrors = null;
+  const { username, email } = req.body;
+  if (!username) validationsErrors = { username: 'Username cannot be null' };
+  if (!email) validationsErrors = { ...validationsErrors, email: 'Email cannot be null' };
+  if (validationsErrors) return res.status(400).json({ validationsErrors });
 
-  res.send({ message: 'User created', userSaved });
-});
+  next();
+};
+
+router.post(
+  '/api/1.0/users',
+  [userValidatorMiddleware],
+  async (req, res) => {
+    const { username, email, password } = req.body;
+    const userSaved = await UserService.save({ username, email, password });
+    res.send({ message: 'User created', userSaved });
+  },
+);
 
 module.exports = router;
